0d5a49e494ab3cd31b68a971c837e24b55856118
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2023-04-30 09:05:07 -0400

    second commit

---
+++ intro/src/index.ts
@@ -0,0 +1,12 @@
+// let message: string = "hello world";
+// console.log(message);
+
+let isPresent: boolean = false;
+let magic: number = 66.6;
+let hello: string = "world";
+
+let notDefined: undefined = undefined;
+let notPresent: null = null;
+
+let penta: symbol = Symbol("star");
+let bigint: bigint = 24n;
