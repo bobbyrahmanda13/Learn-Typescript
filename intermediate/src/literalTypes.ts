5ea537f213626430c5ba03bd0867146d120e3729
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2023-05-28 10:22:08 -0400

    update literal type

--- intermediate/src/literalTypes.ts
+++ intermediate/src/literalTypes.ts
@@ -1,10 +1,41 @@
+// Contoh 0
 // typescript yang biasa
 // let direction: string;
 // direction = "North";
 // direction = "n0r7h";
 
 // literal type
-let direction: "North";
-direction = "North";
-direction = "n0r7h";
+// contoh 1
 
+// let direction: "North";
+// direction = "North";
+// direction = "n0r7h";
+
+// contoh 2
+
+// let direction: "North" | "East" | "South" | "West";
+// direction = "North";
+// direction = "n0r7h";
+// direction = "South";
+
+// contoh 3
+// type CardinalDirection = "North" | "East" | "South" | "West";
+// let direction: CardinalDirection;
+// direction = "North";
+// direction = "n0r7h";
+// direction = "South";
+//
+// function move(distanceMeters: number, direction: CardinalDirection) {
+//   console.log(`Moving ${distanceMeters} meters towards ${direction}`);
+// }
+
+// contoh 4
+// support boolean
+
+type DiceValue = 1 | 2 | 3 | 4 | 5 | 6;
+function rollDice() {
+  return (Math.floor(Math.random() * 6) + 1) as DiceValue;
+}
+if (rollDice() === 7) {
+  throw new Error("Not Possible");
+}
