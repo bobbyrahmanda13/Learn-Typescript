695625038c78e764671172df3540142d00729b88
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2023-08-05 09:55:11 -0400

    function overloading

--- advanced/src/functionOverloading.ts
+++ advanced/src/functionOverloading.ts
@@ -1,3 +1,11 @@
+// function overloading
+// start
+function reverse(string: string): string
+function reverse(stringArray: string[]): string[]
+// end
+
+// body function
+// start
 function reverse(stringOrStringArray: string | string[]) {
   if (typeof stringOrStringArray == "string") {
     return stringOrStringArray.split("").reverse().join("")
@@ -5,6 +13,27 @@ function reverse(stringOrStringArray: string | string[]) {
     return stringOrStringArray.slice().reverse()
   }
 }
+// end
 
 const hello = reverse("hello") // 'olleh'
 const h_e_l_l_o = reverse(["h", "e", "l", "l", "o"]) // ["o", "l", "l", "e", "h"])
+
+// contoh lainnya
+//function overload
+// start
+function makeDate(timestamp: number): Date
+function makeDate(year: number, month: number, day: number): Date
+// end
+
+function makeDate(timestampOrYear: number, month?: number, day?: number): Date {
+  if (month != null && day != null) {
+    return new Date(timestampOrYear, month - 1, day)
+  } else {
+    return new Date(timestampOrYear)
+  }
+}
+
+const doomsday = makeDate(2000, 1, 1) // 1 jan 2000
+const epoch = makeDate(0) // 1 jun 1970
+
+// const invalid = makeDate(2000, 1 /* Error: ignored */) ==> //No overload expects 2 arguments, but overloads do exist that expect either 1 or 3 arguments.ts(2575)
