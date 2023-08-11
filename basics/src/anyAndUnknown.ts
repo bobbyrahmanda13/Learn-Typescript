d82e14b47aa630f5762c89b1b12cddef3f22f9b3
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2023-08-11 10:49:58 -0400

    update format kwkwk

--- basics/src/anyAndUnknown.ts
+++ basics/src/anyAndUnknown.ts
@@ -1,24 +1,23 @@
 //! note any and unknown adalah universal supe typed in typescript
-let exampleAny: any;
-let exampleUnknown: unknown;
-
+let exampleAny: any
+let exampleUnknown: unknown
 
 // any
-exampleAny = 123;
-exampleAny = "hellow";
+exampleAny = 123
+exampleAny = "hellow"
 
 // unknown
-exampleUnknown = 123;
-exampleUnknown = "world";
+exampleUnknown = 123
+exampleUnknown = "world"
 
 // any
-exampleAny.allows.anything.you.can.imagine();
-let anySetBool: boolean = exampleAny;
+exampleAny.allows.anything.you.can.imagine()
+let anySetBool: boolean = exampleAny
 
 // unknown
 if (typeof exampleUnknown == "string") {
-  exampleUnknown.trim();
+  exampleUnknown.trim()
 }
 if (typeof exampleUnknown == "boolean") {
-  let unknownSetBool: boolean = exampleUnknown;
+  let unknownSetBool: boolean = exampleUnknown
 }
