7779bfbf7a3ede531916f8c31d5cdea54e7b6adc
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2023-08-12 10:40:40 -0400

    update index signatures

--- advanced/src/indexSignatures.ts
+++ advanced/src/indexSignatures.ts
@@ -10,6 +10,8 @@ console.log(nums[1337]) // 'leet'
 // syntax => mungkin kwkwk
 type Dictionary = {
   [key: string]: boolean
+  // untuk key bisa di ganti dengan nama bebas
+  // untuk boolean bisa diganti dengan type bebas
 }
 
 // contoh 1
@@ -19,7 +21,8 @@ type PersonIndexSignatures = {
 }
 
 type PersonDictionary = {
-  [username: string]: PersonIndexSignatures
+  [username: string]: PersonIndexSignatures | undefined
+  // jancok: PersonIndexSignatures
 }
 
 const persons: PersonDictionary = {
@@ -27,4 +30,17 @@ const persons: PersonDictionary = {
     displayName: "Jane Doe",
     email: "jane@example.com",
   },
+  // jancok: {
+  //   displayName: "Jane Doe",
+  //   email: "jane@example.com",
+  // },
 }
+
+persons["john"] = { displayName: "John Doe", email: "jhon@example.com" }
+console.log(persons["john"])
+delete persons["john"]
+
+const result = persons["missing"]
+console.log(result, result.email) // undefined, error
+
+persons["john"] = { displayName: "John Doe", emale: "john@example.com" } // error
