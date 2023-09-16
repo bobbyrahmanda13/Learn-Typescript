8f6244b739ecb483cd41b5f6c173c7e68828a7cc
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2023-09-16 09:28:41 +0000

    update keyofOperation

--- expert/keyofTypeOperator.ts
+++ expert/keyofTypeOperator.ts
@@ -88,6 +88,22 @@ const age = logGet(john, "age") // 35
 const names = logGet(john, "name") // 35
 const isTruee = logGet(john, "isTrue") // 35
 const locations = logGet(john, "location") // 35
-console.log(logGet(john, "email")) // Error
+// console.log(logGet(john, "email")) // Error
 // solved step four
 // type PersonKeys = keyof PersonKeyof
+// note : pada functon logGet kan ada 2 parameter yang akan di masukkan yaitu obj dan key
+// nah Obj itu adalah parameter/argumen yang berisi type data object yang nanti dipanggil dalam conth di atas itu variabel nya "john", sedangkan Key itu isi data yg ada dalam objek tersebut dalam hal ini itu adalah name,age,locatin dan isTrue
+// obj dan key itu bisa di ganti dengan yang lain kok jadi gk harus kata Obj / Key saya dah coba sih cuman gk tau dah, dari pemahaman saya seperti itu
+// ini hasil hover nya dari function logGet => function logGet<PersonKeyof, "age">(obj: PersonKeyof, key: "age"): number
+
+function logSet<Obj, Key extends keyof Obj>(
+  obj: Obj,
+  key: Key,
+  value: Obj[Key]
+) {
+  console.log("Setting:", key, value)
+  obj[key] = value
+}
+const jancok = logSet(john, "age", 23)
+
+// begitu juga dengan logSet => function logSet<PersonKeyof, "age">(obj: PersonKeyof, key: "age", value: number): void
