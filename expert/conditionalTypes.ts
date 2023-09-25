21dec3a914fd3c9b98335b6ee48e88ea75c994b0
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2023-09-25 15:05:20 +0000

    update conditional types

--- expert/conditionalTypes.ts
+++ expert/conditionalTypes.ts
@@ -1,8 +1,10 @@
-// testing
-type IsNumber<T>
-  = T extends number
-  ? "number" : "other"
+// example 1
+type IsNumber<T> = T extends number ? "number" : "other"
 // note : T type biasa dipanggil dgn conditional type karena bisa menggunakan conditional operator layaknya if else
+
+// example 
+type WithNumber = IsNumber<number>
+type WithOther = IsNumber<string>
 
 // javascript using typeof conditional
 const isNumber = (value: unknown) =>
@@ -11,3 +13,25 @@ const isNumber = (value: unknown) =>
 const withNumber = isNumber(123)
 const withOther = isNumber("hello")
 
+// example 2
+export type TypeName<T> =
+  T extends string ? 'string' :
+  T extends number ? 'number' :
+  T extends boolean ? 'boolean' :
+  T extends undefined ? 'undefined' :
+  T extends symbol ? 'symbol' :
+  T extends bigint ? 'bigint' :
+  T extends Function ? 'function' :
+  'object'
+
+function typeName<T>(t: T): TypeName<T> {
+  return typeof t as TypeName<T>
+}
+const str = typeName('hello world')
+const num = typeName(123)
+const bool = typeName(true)
+const undef = typeName(undefined)
+const sym = typeName(Symbol('start'))
+const big = typeName(24n)
+const func = typeName(function() { })
+const ob = typeName(null)
