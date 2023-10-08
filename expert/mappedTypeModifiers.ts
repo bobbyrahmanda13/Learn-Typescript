ea77bb201294c385bc1445661dcdd3f588c40745
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2023-10-08 11:19:57 +0000

    update

--- expert/mappedTypeModifiers.ts
+++ expert/mappedTypeModifiers.ts
@@ -2,8 +2,13 @@ export type Point = {
   readonly x: number,
   y?: number
 }
+
 export type Mapped<T> = {
-  readonly [P in keyof T]: T[P]
+  // readonly [P in keyof T]: T[P]
+
+  // optional type 
+  +readonly [P in keyof T]: T[P]
 }
 
 export type Result = Mapped<Point>
+
