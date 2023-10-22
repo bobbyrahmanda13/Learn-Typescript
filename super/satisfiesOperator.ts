3dae078115d0d1d017cdbb5f7e3ab61e6b6a9fd6
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2023-10-22 03:19:13 +0000

    update satisfied operator

--- super/satisfiesOperator.ts
+++ super/satisfiesOperator.ts
@@ -1,1 +1,21 @@
-#
+type Color = ColorString | ColorRGB
+type ColorString = 'red' | 'blue' | 'yellow' | 'purple'
+type ColorRGB = [red: number, green: number, blue: number]
+
+type Theme = Record<string, Color>
+
+// before using satisfies
+// const theme: Theme = {
+//   primary: 'red',
+//   secondary: [0, 255, 0],
+//   tertiary: 'purpel', // error: karena typo yang harus nya purple tapi yang dibuat adalah purpel
+// }
+
+// after using satisfies
+const theme = {
+  primary: 'red',
+  secondary: [0, 255, 0],
+  tertiary: 'purple',
+}
+
+const [r, g, b] = theme.secondary
